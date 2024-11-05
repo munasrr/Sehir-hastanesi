4757d6ab4e712f37b8d85b9e650e2aff03d56da6
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-05 20:56:21 +0300

    fixed tailwind configts/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans} from "next/font/local";
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import "./globals.css";
 import { cn } from '../lib/utils'
 
 const fontSans = Plus_Jakarta_Sans({
-  subset:["latin"],
-  weight: ['300','400','500','600','700'],
-  variable:'---font-Sans',
+  subsets: ['latin'], // Note 'subsets' instead of 'subset'
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans', // Make sure this variable name is consistent with your CSS
 });
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',fontSans.variable)}>
         {children}
       </body>
     </html>
