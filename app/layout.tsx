cd6f730decc686943179cc1f52fbe60edb3dcd14
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-05 20:03:12 +0300

    feat:formatDateAndTime to use client time zone

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Plus_Jakarta_Sans} from "next/font/local";
 import "./globals.css";
+import { cn } from '../lib/utils'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontSans = Plus_Jakarta_Sans({
+  subset:["latin"],
+  weight: ['300','400','500','600','700'],
+  variable:'---font-Sans',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={cn}>
         {children}
       </body>
     </html>
