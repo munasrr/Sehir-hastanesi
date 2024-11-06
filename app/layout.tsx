cc23f7adb17e31f934be463079c8687db912c4fd
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-06 15:11:36 +0300

    Add Patient Form and Passkey Modal to Home component; Enhance UI structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from 'next/font/google';
-import "./globals.css";
-import { cn } from '../lib/utils'
+import type { Metadata } from 'next';
+import './globals.css';
+import { Plus_Jakarta_Sans as FontSans } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
 
-const fontSans = Plus_Jakarta_Sans({
-  subsets: ['latin'], 
+import { cn } from '@/lib/utils';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
-  title: 'Şehir hastanesi',
-  description: 'Health care Management system',
+  title: 'Sehir',
+  description:
+    'A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.',
+  icons: {
+    icon: '/assets/icons/logo-icon.svg',
+  },
 };
 
 export default function RootLayout({
@@ -21,8 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',fontSans.variable)}>
-        {children}
+      <body
+        className={cn(
+          'min-h-screen bg-dark-300 font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
