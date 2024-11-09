6c549721324aa7ac2623be196cf6c58b30197b9c
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-10 00:56:07 +0300

    feat: added admin section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-
+import PatientForm from '../components/forms/PatientForm';
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
@@ -7,13 +8,31 @@ export default function Home() {
         <div className="sub-container max-w-[496]">
           <Image
             src="/assets/icons/logo-full.svg"
-            alt="Patient"
             width={1000}
             height={1000}
+            alt="Patient"
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+
+            <p className='justify-items-end text-dark-600 xl:test-left'> © 2024 Sehir hastanesi</p>
+
+            <Link href="/?admin=true" className="text-green-500">
+            Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+    <Image
+    src="/assets/images/onboarding-img.png"
+    height={1000}
+    width={1000}
+    alt='Patient'
+    className='side-img max-w-w-[50%]'
+    />
     </div>
   );
 }
