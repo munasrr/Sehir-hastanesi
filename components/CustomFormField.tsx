cdf54d68635943fc4628ae5627f56912ec86b570
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-11-10 18:16:04 +0300

    Refactor CustomFormField component for reusability and flexibility with dynamic props

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,9 +1,28 @@
 'use client'
-import React from 'react'
+import {FormControl,FormDescription,FormField,FormItem,FormLabel, FormMessage,} from '../../components/ui/form';
+import { Input } from '../../components/ui/input';
+import { Control } from 'react-hook-form';
+import { FormFieldType } from './forms/PatientForm';
+ interface CustomProps {
+    control:Control<any>,
+    fieldType:FormFieldType
+}
 
-
-export const CustomFormField = () => {
+export const CustomFormField = ({control,fieldType}:CustomProps) => {
   return (
-    <div>CustomFormField</div>
-  )
+    <FormField
+      control={control}
+      name="username"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>This is your public display name.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 }
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,17 +3,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '../../components/ui/form';
-import { Input } from '../../components/ui/input';
+import { Form } from '../../components/ui/form';
 import { Button } from '../ui/button';
+import { CustomFormField } from '../CustomFormField';
+
+export enum FormFieldType {
+    INPUT = 'input',
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -37,23 +33,14 @@ const PatientForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1>Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appoinment.</p>
+        </section>
+        <CustomFormField
+         fieldType={FormFieldType.INPUT} 
+         control={form.control} />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
