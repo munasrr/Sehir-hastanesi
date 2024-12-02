ee0bbb82765e800edbbd505d2d9974e00e03ebe9
Anwar Abdi <Anwarabdi2090@gmail.com>
2024-12-02 22:00:39 +0300

    page.tsx Todo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      {/* Todo: OTP Verification | passkey model */}
+      {/* Todo: OTP Verification | passkey models */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
